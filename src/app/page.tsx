72c01bba337fa36d0fef63fe680492d10df8f4af
Benjamin Raffetseder <hello@benjamin-raffetseder.com>
2024-01-03 19:12:10 +0100

    + simple landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 import { css } from 'styled-system/css'
-import { Container } from 'styled-system/jsx'
+import { Container, HStack } from 'styled-system/jsx'
 import { Button } from '~/components/ui/button'
 
 export default function Home() {
@@ -9,17 +9,38 @@ export default function Home() {
     bg: 'white.a10'
    })}
    >
-    <Container>
+    <Container
+      className={css({
+        py: 5,
+        px: 4,
+        maxWidth: 720,
+        height: '100vh',
+        mx: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        gap: 2,
+      })}
+    >
       <h1 className={css({
         fontSize: 40,
         fontWeight: 700,
       })}>
        Markee
       </h1>
+      <h2 className={css({
+        fontSize: 24,
+        fontWeight: 500,
+      })}>
+        Your reading companion
+      </h2>
       <p>
         Markee is a simple app that wants to help you to keep track of your reading progress on your physical books. Add your books, create book-specific notes, or to keep quotes in one place. Forget your bookmarks.
       </p>
-      <Button variant="solid">Get started</Button>
+      <HStack gap={10} pt={2}>
+        <Button variant="solid" className={css({w: 'fit-content'})} >Get started</Button>
+        <Button variant="ghost" className={css({ w: 'fit-content' })} >About</Button>
+      </HStack>
         </Container>
     </main>
   )
