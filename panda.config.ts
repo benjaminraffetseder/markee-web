86a5bec528638e9db2481f7822da89e69ba6b397
Benjamin Raffetseder <hello@benjamin-raffetseder.com>
2024-01-03 18:58:01 +0100

    + added all park ui components

--- panda.config.ts
+++ panda.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from '@pandacss/dev'
+import { createPreset } from '@park-ui/panda-preset'
+import { button } from '~/recipes/button'
 
 export default defineConfig({
   preflight: true,
-  presets: ['@pandacss/preset-base', '@park-ui/panda-preset'],
+  presets: ['@pandacss/preset-base', '@park-ui/panda-preset',
+    createPreset({
+      accentColor: 'grass',
+      grayColor: 'olive',
+      borderRadius: 'sm',
+    }),
+  ],
+  theme: {
+    extend: {
+      recipes: {
+        button,
+      },
+    },
+  },
   include: ['./src/**/*.{js,jsx,ts,tsx}'],
   exclude: [],
   jsxFramework: 'react',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-import { Button } from '@/components/button'
-import Image from 'next/image'
+
 import { css } from 'styled-system/css'
+import { Container } from 'styled-system/jsx'
+import { Button } from '~/components/ui/button'
 
 export default function Home() {
   return (
-   <main>
+   <main className={css({
+    bg: 'white.a10'
+   })}
+   >
+    <Container>
       <h1 className={css({
         fontSize: 40,
+        fontWeight: 700,
       })}>
-        Welcome to Markee
+       Markee
       </h1>
+      <p>
+        Markee is a simple app that wants to help you to keep track of your reading progress on your physical books. Add your books, create book-specific notes, or to keep quotes in one place. Forget your bookmarks.
+      </p>
+      <Button variant="solid">Get started</Button>
+        </Container>
     </main>
   )
 }
